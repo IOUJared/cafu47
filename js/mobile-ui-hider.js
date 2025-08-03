04f35defe99e95f36acaf928fc09ab6d1229bd8d
Jared Scheer <jaredjscheer@gmail.com>
2025-08-03 05:35:44 -0500

    fixing safari

--- js/mobile-ui-hider.js
+++ js/mobile-ui-hider.js
@@ -4,6 +4,16 @@ export class MobileBrowserUIHider {
         this.mainContainer = document.getElementById('main-container');
         this.isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
+        if (this.isIOS) {
+            // Add a helper class to the body to create a scroll buffer
+            document.body.classList.add('ios-scroll-helper');
+
+            // Attempt to hide the UI on load
+            setTimeout(() => {
+                window.scrollTo(0, 1);
+            }, 300);
+        }
+
         if (this.fullscreenButton && this.mainContainer) {
             this.setupEventListeners();
         }
@@ -17,18 +27,15 @@ export class MobileBrowserUIHider {
 
     handleMobileAction() {
         if (this.isIOS) {
-            // iOS does not support the Fullscreen API.
-            // We will scroll to hide the URL bar and then hide the button.
+            // On iOS, scroll to hide the URL bar and then hide the button
             window.scrollTo(0, 1);
             this.fullscreenButton.style.display = 'none';
         } else {
-            // For Android and other supported browsers, use the Fullscreen API.
-            if (!document.fullscreenElement && !document.webkitFullscreenElement) {
-                if (this.mainContainer.requestFullscreen) {
-                    this.mainContainer.requestFullscreen();
-                } else if (this.mainContainer.webkitRequestFullscreen) { /* Safari */
-                    this.mainContainer.webkitRequestFullscreen();
-                }
+            // For Android, use the Fullscreen API
+            if (this.mainContainer.requestFullscreen) {
+                this.mainContainer.requestFullscreen();
+            } else if (this.mainContainer.webkitRequestFullscreen) {
+                this.mainContainer.webkitRequestFullscreen();
             }
         }
     }
