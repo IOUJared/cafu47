a2650521590566ab6f98572d2c6df854d21bfd0a
Jared Scheer <jaredjscheer@gmail.com>
2025-08-03 05:29:26 -0500

    fixing safari

--- js/mobile-ui-hider.js
+++ js/mobile-ui-hider.js
@@ -2,6 +2,7 @@ export class MobileBrowserUIHider {
     constructor() {
         this.fullscreenButton = document.getElementById('fullscreen-btn');
         this.mainContainer = document.getElementById('main-container');
+        this.isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 
         if (this.fullscreenButton && this.mainContainer) {
             this.setupEventListeners();
@@ -10,21 +11,24 @@ export class MobileBrowserUIHider {
 
     setupEventListeners() {
         this.fullscreenButton.addEventListener('click', () => {
-            this.enterFullscreen();
+            this.handleMobileAction();
         });
-
-        // Also add a fallback for initial scroll hide
-        setTimeout(() => {
-            window.scrollTo(0, 1);
-        }, 300);
     }
 
-    enterFullscreen() {
-        if (!document.fullscreenElement && !document.webkitFullscreenElement) {
-            if (this.mainContainer.requestFullscreen) {
-                this.mainContainer.requestFullscreen();
-            } else if (this.mainContainer.webkitRequestFullscreen) { /* Safari */
-                this.mainContainer.webkitRequestFullscreen();
+    handleMobileAction() {
+        if (this.isIOS) {
+            // iOS does not support the Fullscreen API.
+            // We will scroll to hide the URL bar and then hide the button.
+            window.scrollTo(0, 1);
+            this.fullscreenButton.style.display = 'none';
+        } else {
+            // For Android and other supported browsers, use the Fullscreen API.
+            if (!document.fullscreenElement && !document.webkitFullscreenElement) {
+                if (this.mainContainer.requestFullscreen) {
+                    this.mainContainer.requestFullscreen();
+                } else if (this.mainContainer.webkitRequestFullscreen) { /* Safari */
+                    this.mainContainer.webkitRequestFullscreen();
+                }
             }
         }
     }
